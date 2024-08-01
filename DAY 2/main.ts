b18aa6e3e4552bff4dd87391f91d1419c8e5065c
mahnooransari78 <164240218+mahnooransari78@users.noreply.github.com>
2024-08-01 23:02:44 +0500

    intial commit

---
+++ DAY 2/main.ts
@@ -0,0 +1,25 @@
+// Question 1:
+
+// Famous Quote: find a quote from a famouse person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// "Albert Einstein once said, "A person who never made a mistake never tride anything new" .
+
+console.log("Albert Einstein once said, 'A person who never made a mistake never tride anything new' ");
+
+// Question : 2 
+
+// Famous Quote 2 : Repeat Exerecise 1, but this time store the famous person's name in a variable called famous_person. then compose your message and store it in a new veriable called message. Print your message
+
+let famous_person : string = "Albert Einstein";
+
+let message : string = `${famous_person} once said, 'A person who never made a mistake never tride anything new'`;
+
+console.log(message);
+
+// Question : 3 
+
+// stripping Name: store a person's name, and include some whitespace characters at the beginning and end of the name. Use"\t"(tab)and "\n"(new line) at least once. Print the name after stripping the ehite spaces.
+
+let person_name : string = "\t \n Mahnoor\t\n";
+console.log(person_name);
+console.log(person_name.trim());
+
---
+++ Day 1/main.ts
@@ -0,0 +1,20 @@
+//  Question : 1
+
+
+// Personal Message: Store a person's name in variable and print a message to them, like "Hello Mahnoor would you like learn some TypeScript to day? ".
+const person_name = "Mahnoor";
+console.log(`Hello ${person_name} would you like learn some TypeScript to day?`);
+
+// Question: 2
+// Name Cases : Store a person's name in a veriable, and then print thet person's name in lowercase, upercase, and titlecase.
+
+let personNmer = "mahnoor";
+
+console.log(personNmer.toLowerCase()); // show the name in all small letters.
+
+console.log(personNmer.toUpperCase());//show the name in all CAPITAL LETTER.
+
+console.log(personNmer.charAt(0).toUpperCase()+personNmer.slice(1).toLowerCase());//show the name in all First letters Big.
+
+
+
